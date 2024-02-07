b074b6752b04ec7307f4640a97a6aecd7722e6c0
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2024-02-07 07:31:35 -0500

    Updates

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Hugo is the BEST!!!</Text>
+            </View>
+            <Text
+                style={{
+                    margin: 16,
+                    borderWidth: 1,
+                    borderColor: "red",
+                    padding: 16,
+                }}
+            >
+                This is a test
+            </Text>
+            <Button title="Press Me!" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });

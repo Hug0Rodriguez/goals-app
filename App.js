50f77a9625ef20ddc0fca772f51c5a350f744fe1
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2024-02-13 07:55:13 -0500

    added functionality
    The app is now able to add a goal

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+    const [enteredGoalText, setEnteredGoalText] = useState("");
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+    function addGoalHandler() {
+        setCourseGoals((currentCourseGoals) => [
+            ...currentCourseGoals,
+            enteredGoalText,
+        ]);
+    }
     return (
         <View style={styles.appContainer}>
             <View style={styles.inputContainer}>
                 <TextInput
                     style={styles.textInput}
                     placeholder="Your Course goal!"
+                    onChangeText={goalInputHandler}
                 />
-                <Button color={"purple"} title="Add Goal" />
+                <Button
+                    color={"purple"}
+                    title="Add Goal"
+                    onPress={addGoalHandler}
+                />
             </View>
             <View style={styles.goalsContainer}>
-                <Text>List Of Goals...</Text>
+                {courseGoals.map((goal) => (
+                    <View>
+                        /
+                        <Text style={styles.goalItem} key={goal}>
+                            {goal}
+                        </Text>
+                    </View>
+                ))}
             </View>
         </View>
     );
@@ -41,5 +66,12 @@ const styles = StyleSheet.create({
     },
     goalsContainer: {
         flex: 4,
+    },
+    goalItem: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: "#5e0acc",
+        color: "white",
     },
 });
